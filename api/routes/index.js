b5bedc07af785ed68b17110437d18de6401e76b2
Michael Wolf Hoffman <michaelhoffmanj@gmail.com>
2022-04-27 08:37:50 -0600

    started mongo container

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/test", function (req, res, next) {
-  res.send({ message: "it is working." });
+  res.send({ message: "This is a message coming from the API container. Docker is working!" });
 });
 
 module.exports = router;
