5db21a582d9125ad477272ff1be4878b776731c3
Michael Wolf Hoffman <michaelhoffmanj@gmail.com>
2022-04-28 16:00:03 -0600

    starting nginx.

--- api/app.js
+++ api/app.js
@@ -4,7 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var mongoose = require('mongoose');
-
+var cors = require("cors");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
@@ -21,7 +21,7 @@ db.once('open', ()=>{
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
-
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
